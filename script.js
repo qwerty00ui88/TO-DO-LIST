495ebeb835235a60e2af344fd53a3d829eb94e49
qwerty00ui88 <qwerty0.0ui88@gmail.com>
2023-01-08 00:42:34 +0900

    체크 여부 저장/input,label 연결

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const button = document.querySelector('button');
 const textarea = document.querySelector('textarea');
 const i = document.querySelectorAll('i');
 
-const convertToDom = (text, index) => {
+const convertToDom = (text, check, index) => {
   const li = document.createElement('li');
   const input = document.createElement('input');
   const label = document.createElement('label');
@@ -15,12 +15,16 @@ const convertToDom = (text, index) => {
 
   li.setAttribute('id', index);
   input.setAttribute('type', 'checkbox');
+  input.setAttribute('id', `checkbox_${index}`);
+  input.addEventListener('click', handleCheck);
+  check ? input.setAttribute('checked', '') : null; // 새로고침 시 check 여부 기억
+  label.setAttribute('for', `checkbox_${index}`);
   label.textContent = text;
   i.setAttribute('class', `fa-solid fa-xmark`);
   i.addEventListener('click', deleteLi);
 
-  ul.append(li);
   li.append(input, label, i);
+  ul.append(li);
 };
 
 button.addEventListener('click', () => {
@@ -28,21 +32,28 @@ button.addEventListener('click', () => {
     alert('할 일 없어?');
   } else {
     // data와 localStorage 배열에 추가
-    data.push(textarea.value);
+    data.push({ text: textarea.value, check: false });
     localStorage.setItem('localData', JSON.stringify(data));
     // 새로운 list의 DOM 추가
-    convertToDom(textarea.value, data.length);
+    convertToDom(textarea.value, false, data.length - 1);
     textarea.value = '';
   }
 });
 
 const deleteLi = (e) => {
-  // data와 localStorage 배열에서 삭제
+  // data와 localStorage에서 삭제
   let targetIndex = Number(e.target.parentElement.id);
   data.splice(targetIndex, 1);
   localStorage.setItem('localData', JSON.stringify(data));
   // 리렌더링
   render();
+};
+
+const handleCheck = (e) => {
+  // data와 localStorage 변경
+  let targetIndex = Number(e.target.parentElement.id);
+  data[targetIndex].check = e.target.checked;
+  localStorage.setItem('localData', JSON.stringify(data));
 };
 
 const render = () => {
@@ -51,7 +62,7 @@ const render = () => {
     ul.removeChild(ul.firstChild);
   }
   // 새로운 DOM 생성
-  data.forEach((el, index) => convertToDom(el, index));
+  data.forEach((el, index) => convertToDom(el.text, el.check, index));
 };
 
 render();
